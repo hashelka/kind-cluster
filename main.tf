59232dd54c3afd1302a6bbe3f16bd3ab23b0bc60
Elvira <vetelvira@gmail.com>
2026-06-25 13:31:22 +0200

    tf

--- main.tf
+++ main.tf
@@ -4,7 +4,18 @@ terraform {
       source  = "tehcyx/kind"
       version = "~> 0.6.0"
     }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.0"
+    }
   }
+}
+
+provider "kubernetes" {
+  host                   = kind_cluster.learning.endpoint
+  client_certificate     = kind_cluster.learning.client_certificate
+  client_key             = kind_cluster.learning.client_key
+  cluster_ca_certificate = kind_cluster.learning.cluster_ca_certificate
 }
 
 provider "kind" {}
