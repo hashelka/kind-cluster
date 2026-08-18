1d61f15c1badb93845cb88ea50b0956c90ae5d1a
Elvira <vetelvira@gmail.com>
2026-08-18 20:08:17 +0200

    test

--- argocd.tf
+++ argocd.tf
@@ -14,6 +14,5 @@ resource "helm_release" "argocd" {
   repository = "https://argoproj.github.io/argo-helm"
   chart      = "argo-cd"
   namespace  = kubernetes_namespace.argocd.metadata[0].name
-
   depends_on = [kubernetes_namespace.argocd]
 }
